2eec1e9c8ef6b2b4fe38c6efcd23a94d41d90673
CodeByMonir <monirhossen39682@gmail.com>
2026-03-07 15:44:19 +0600

    log in page done successfully

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('connected')
+
+
+function submit() {
+    const userName = document.getElementById('username');
+    const user = userName.value;
+
+    const password = document.getElementById('password');
+    let pass = password.value;
+
+    if (user !== "admin" && user.length !== '5') {
+        alert('Wrong Username')
+    }
+    else if (pass !== "admin123" && pass.length !== '8') {
+        alert('Wrong Password')
+    }
+    else {
+        alert("Log In Successful");
+        window.location.assign("./home.html");
+    }
+}
