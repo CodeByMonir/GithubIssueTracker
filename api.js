85191041866aa7addf90d76dc78f1e93214eb05d
CodeByMonir <monirhossen39682@gmail.com>
2026-03-08 20:51:33 +0600

    btns toggling done status counts done open close functions are added

--- api.js
+++ api.js
@@ -1,27 +1,107 @@
 const mainSec = document.getElementById('main-sec');
-const spinner = document.getElementById('spinner')
+const spinner = document.getElementById('spinner');
 
-async function loadData() {
+// button call
+const allBtn = document.getElementById('all-btn');
+const openBtn = document.getElementById('open-btn');
+const closedBtn = document.getElementById('closed-btn');
+
+// issue count call
+const issueCount = document.getElementById('issue-count');
+
+// search bar call
+const search = document.getElementById('search-bar');
+const searchBar = search.value;
+
+
+// button toggling
+
+allBtn.addEventListener('click', function () {
+    allBtn.classList.add('btn-primary');
+    openBtn.classList.remove('btn-primary');
+    closedBtn.classList.remove('btn-primary');
+
+    loadData();
+});
+
+openBtn.addEventListener('click', function () {
+    allBtn.classList.remove('btn-primary');
+    openBtn.classList.add('btn-primary');
+    closedBtn.classList.remove('btn-primary');
+
+    openData();
+});
+
+closedBtn.addEventListener('click', function () {
+    allBtn.classList.remove('btn-primary');
+    openBtn.classList.remove('btn-primary');
+    closedBtn.classList.add('btn-primary');
+
+    closedData();
+});
+
+
+
+function openSpinner() {
     spinner.classList.remove("hidden");
-    spinner.classList.add('flex');
+    mainSec.innerHTML = "";
+};
+
+function closeSpinner() {
+    spinner.classList.add("hidden");
+};
+
+
+
+async function openData() {
+    openSpinner();
 
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
-
     const load = await res.json();
 
-    spinner.classList.remove("flex");
-    spinner.classList.add('hidden');
+    const openData = load.data.filter(issue => issue.status === "open");
 
-    displayData(load.data)
-    ocBtn(load.data.status)
+    displayData(openData);
+
+    issueCount.textContent = openData.length;
+
+    closeSpinner();
 }
 
-async function ocBtn(params) {
-    console.log(params);
+
+async function closedData() {
+    openSpinner();
+
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+    const load = await res.json();
+
+    const closedData = load.data.filter(issue => issue.status === "closed");
+
+    displayData(closedData);
+
+    issueCount.textContent = closedData.length;
+
+    closeSpinner();
+}
+
+async function loadData() {
+
+    openSpinner();
+
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+    const load = await res.json();
+
+    issueCount.textContent = load.data.length;
+
+    closeSpinner();
+
+    displayData(load.data);
 }
 
 function displayData(data) {
     data.forEach((data) => {
+
+        // console.log(data.status)
 
         const card = document.createElement('div');
         card.className = "card border-t-3 border-t-[#00A96E] shadow-md p-4 rounded-xl";
