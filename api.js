0d6af6a7361d77ff38657b64db53fa9b276741de
CodeByMonir <monirhossen39682@gmail.com>
2026-03-09 05:20:18 +0600

    modal code done but connection is not complete

--- api.js
+++ api.js
@@ -156,7 +156,7 @@ function displayData(data) {
 
         const card = document.createElement('div');
         card.className = `card border-t-3 border-t-${data.status === "open" ? '[#00A96E]' : '[#A855F7]'} shadow-md p-4 rounded-xl`;
-        card.setAttribute("onclick", `modal(${data.id})`);
+        card.setAttribute("onclick", `modal(${data.id}, ${JSON.stringify(data)})`);
 
         card.innerHTML = `<div class="flex items-center justify-between mb-4">
 
@@ -191,6 +191,57 @@ function displayData(data) {
 loadData();
 
 
-function modal(id){
+function modal(id, data){
+
+    console.log(id, data);
+
+    // data.forEach((data) => {
+    //     let priorityColor = "bg-[#EEEFF2] text-[#9CA3AF]";
+
+    //     if (data.priority === "high") {
+    //       priorityColor = "bg-[#FEECEC] text-[#EF4444]";
+    //     } else if (data.priority === "medium") {
+    //       priorityColor = "bg-[#FFF6D1] text-[#F59E0B]";
+    //     }
+
+    //     const card = document.createElement("div");
+
+    //     card.innerHTML = `
+    //         <h1 class="text-[#1F2937] font-bold text-[24px]">${data.title}</h1>
+    //         <div class="flex items-center gap-2 text-[12px] text-[#64748B]">
+    //             <button class="${data.status === "open" ? "bg-[#00A96E]" : "bg-[#A855F7]"} py-1 px-2    rounded-full -2 text-white">${data.status}</button> 
+    //             <div class="h-1 w-1 bg-[#64748B] rounded-full"></div> 
+    //             <p>Opened by ${data.assignee}</p> 
+    //             <div class="h-1 w-1 bg-[#64748B] rounded-full"></div>
+    //             <p>${data.updatedAt}</p>
+    //         </div>
+    //         <div class="flex gap-2 mb-6">
+    //             ${data.labels.map((tag) => `
+    //                 <span class="text-xs px-3 py-1 rounded-full 
+    //                 ${tag === "bug" ? "bg-red-100 text-red-600 border border-red-200" : tag === "enhancement" ?     "bg-green-100 text-green-600 border border-green-200" : tag === "help wanted" ? "bg-yellow-100  text-yellow-600 border border-yellow-200" : tag === "good first issue" ? "bg-gray-100    text-gray-600 border border-gray-200" : tag === "documentation" ? "bg-blue-100 text-blue-600   border border-blue-200" : ""}">${tag}</span>
+    //             `,).join("")}
+    //         </div>
+    //         <p class="text-[#64748B]">${data.description}</p>
+    //         <div class="p-4 bg-[#F8FAFC] rounded-md flex">
+    //             <div class="w-[296px]">
+    //                 <p class="text-[#64748B]">Assignee:</p>
+    //                 <p class="text-[#1F2937] font-semibold">${data.assignee}</p>
+    //             </div>
+    //             <div class="w-[296px]">
+    //                 <p class="text-[#64748B]">Priority:</p>
+    //                 <h3 class=" w-[80px] h-6 rounded-xl ${priorityColor} text-center">${data.priority}</h3>
+    //             </div>                                
+    //         </div>
+    //         <div class="modal-action">
+    //             <form method="dialog"> 
+    //                 <!-- if there is a button in form, it will close the modal -->
+    //                 <button class="btn btn-primary">Close</button>
+    //             </form>
+    //         </div>
+    //     `;
+    // });
+    
+
+        
     
 }
